5dd6bcc803ca415f68588d76fc566515a7968d71
pihizi <pihizi@msn.com>
2016-09-12 09:12:31 +0000

    如果更新的是自己，还是应该允许更新的

--- class/Gini/Controller/CGI/AJAX/Settings/BPM.php
+++ class/Gini/Controller/CGI/AJAX/Settings/BPM.php
@@ -189,8 +189,8 @@ class BPM extends \Gini\Controller\CGI
             if (!$group->id) {
                 return \Gini\IoC::construct('\Gini\CGI\Response\JSON', T('操作失败'));
             }
-            $group  = $process->getGroup($name);
-            if ($group->id) {
+            $newgroup  = $process->getGroup($name);
+            if ($newgroup->id && $newgroup->id!=$group->id) {
                 return \Gini\IoC::construct('\Gini\CGI\Response\JSON', T('操作失败, 分组标识已被占用'));
             }
         } else {
