25e5e46838cd6cf05fbbe14f5eae2606a159d394
wenjun zheng <wenjun.zheng@geneegroup.com>
2017-05-26 09:44:45 +0000

    回调函数，处理订单

--- class/Gini/Controller/CGI/Rest/Order.php
+++ class/Gini/Controller/CGI/Rest/Order.php
@@ -7,17 +7,49 @@ class Order extends \Gini\Controller\CGI\Layout
 {
     public function actionApprove()
     {
-        error_log('pass');
-        $post = $this->form('post');
-        error_log(J($post));
+        $content = file_get_contents('php://input');
+        $order_data = json_decode($content);
 
+        $order = a('order');
+        $order->setData($order_data);
+
+        $voucher = $order_data['voucher'];
+        if (!$voucher) return;
+        $rpc = self::_getRPC('order');
+        if (!$rpc) return;
+        $now = date('Y-m-d H:i:s');
+        try {
+            $bool = $rpc->mall->order->updateOrder($voucher, [
+                'status' => $order->customized ? \Gini\ORM\Order::STATUS_APPROVED : \Gini\ORM\Order::STATUS_NEED_VENDOR_APPROVE,
+                'payment_status'=> \Gini\ORM\Order::PAYMENT_STATUS_PENDING,
+                'description'=> [
+                    'a' => $order->customized ? T('订单被审批通过, 可以进行付款') : T('订单交给供应商确认'),
+                    't' => $now,
+                ]
+            ]);
+        } catch (\Exception $e) {
+        }
     }
 
     public function actionReject()
     {
-        error_log('reject');
-        $post = $this->form('post');
-        error_log(J($post));
-
+        $content = file_get_contents('php://input');
+        $order_data = json_decode($content);
+        $voucher = $order_data['voucher'];
+        if (!$voucher) return;
+        $rpc = self::_getRPC('order');
+        if (!$rpc) return;
+        $now = date('Y-m-d H:i:s');
+        try {
+            $bool = $rpc->mall->order->updateOrder($voucher, [
+                'status' => \Gini\ORM\Order::STATUS_CANCELED,
+                'description'=> [
+                    'a' => T('**系统** 自动 **取消** 了该订单'),
+                    't' => $now,
+                ]
+            ]);
+        } catch (\Exception $e) {
+        }
     }
 }
+
