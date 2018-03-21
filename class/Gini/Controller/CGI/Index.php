073cd7d5c17cbc16557c4473a40ec8f2d28f3939
xuguang.chen <xuguang.chen@geneegroup.com>
2018-03-21 10:52:57 +0800

    新前端要上线了，左边栏点进去就需要跳转到新首页了

--- class/Gini/Controller/CGI/Index.php
+++ class/Gini/Controller/CGI/Index.php
@@ -5,7 +5,7 @@ namespace Gini\Controller\CGI;
 class Index extends Layout\Board{
 
     public function __index(){
-        $this->redirect('pending');
+        $this->redirect(\Gini\Module\AdminBase::getFEUrl('order/review'));
     }
 
     public function actionLogout()
